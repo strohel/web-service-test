8c5063c3b33bf611b035ac90c4a01f7710db6b7f
Matěj Laitl <matej@laitl.cz>
2010-10-21 18:09:59 +0200

    komentare v startweb2py.sh

--- startweb2py.sh
+++ startweb2py.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# cesta k instalaci web2py:
 WEB2PY_PATH="${HOME}/škola/4. ročník/OOP/projekt/web2py"
 
+# nastartovat server, ktery posloucha vsude, nezobrazi gui, heslo nastavit na veslo, a nestartuje cron
 cd "${WEB2PY_PATH}"
 ./web2py.py -i 0.0.0.0 --nogui -a veslo -N
