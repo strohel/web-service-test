9ea17f4bff69f4c89e8f958fea9dbbc1aac8632b
Matěj Laitl <matej@laitl.cz>
2010-11-04 10:18:55 +0100

    prenositelnejsi startovaci skript pro web2py

--- startweb2py.sh
+++ startweb2py.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # cesta k instalaci web2py:
-WEB2PY_PATH="${HOME}/škola/4. ročník/OOP/projekt/web2py"
+WEB2PY_PATH="./web2py"
 
 # nastartovat server, ktery posloucha vsude, nezobrazi gui, heslo nastavit na veslo, a nestartuje cron
-cd "${WEB2PY_PATH}"
-./web2py.py -i 0.0.0.0 --nogui -a veslo -N
+pushd "${WEB2PY_PATH}"
+./web2py.py -i 0.0.0.0 --nogui -a veslo -N &
+popd
